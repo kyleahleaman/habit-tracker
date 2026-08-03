d4222212d0d17cc4bcf8eb0d2b4c2418a150410e
Emma Lang <LANG@etown.edu>
2026-08-03 10:53:45 -0400

    update

--- aboutandpurpose.php
+++ aboutandpurpose.php
@@ -16,7 +16,9 @@ include "navbar.php";
     <h3>Sophia- Sophia is awesome and likes pink</h3>
     <h3>Paige- Paige hates coding and loves eating</h3>
     <h3>Kyleah- Kyleah is the best!</h3>
-    <h3>Emma- Coding is a recent passion of mine that I gained from being forced to join a random elective at school. This world is newer to me, but I am happy to learn all that I can, which is why I chose to join this program and stick with it to the very end of this project.</h3>
+    <h3>Emma- Coding is a recent passion of mine that I gained from being forced to join a random 
+        elective at school. This world is newer to me, but I am happy to learn all that I can, which is 
+        why I chose to join this program and stick with it to the very end of this project.</h3>
 
 </body>
 </html>

ac9d2b5ff75d148d432211075564e9533f164aba
Kyleah Leaman <kyleah.leaman@gmail.com>
2026-08-03 11:31:07 -0400

    db config and nav/footers adjustments

--- footer.php
+++ footer.php
@@ -1,5 +1,46 @@
-<div class='w3-white w3-padding w3-card w3-bottom'>
-    <ul>
-        <li><a href="aboutandpurpose.php">About Us</a></li>
-    </ul>
-</div>
+<html lang="en">
+<head>
+    <style>
+        ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+            overflow: hidden;
+            background-color: #91c7b1;
+        }
+
+        li {
+            float: left;
+        }
+
+        li a {
+            display: block;
+            color: #2a2b2e;
+            padding: 14px 26px;
+            text-decoration: none;
+            font-size: 17px;
+            
+            
+        }
+
+        li a:hover {
+            background-color: rgb(236, 193, 199);
+        }
+
+    </style>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <div class='w3-bottom'>
+                    <ul>
+                        <li><a href="aboutandpurpose.php">About Us</a></li>
+                    </ul>
+                </div>
+
+            </ul>
+        </nav>
+    </header>
+</body>
+</html>
--- processes/dbconfig.php
+++ processes/dbconfig.php
@@ -1,6 +1,6 @@
 <?php 
-    $servername = ''; 
-    $username = '';
-    $password = '';
-    $database = '';
+    $servername = 'srv1482.hstgr.io'; 
+    $username = 'u121755072_habbittracker';
+    $password = 'Q7g:0|Km6';
+    $database = 'u121755072_habbitdb';
 ?>

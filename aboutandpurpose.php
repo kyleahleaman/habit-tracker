60139ed67091b338b11bd0ad84a2d9efdf6a7292
Emma Lang <LANG@etown.edu>
2026-08-03 10:38:42 -0400

    updating my about me paragraph

--- aboutandpurpose.php
+++ aboutandpurpose.php
@@ -1,5 +1,5 @@
 <?php
-include "header.php";
+include "navbar.php";
 ?>
 <html lang="en">
 <head>
@@ -10,13 +10,13 @@ include "header.php";
 </head>
 <body>
     <h1>Our Purpose</h1>
-    <h4>The purpose of this website it to build meaningful, 
-        lasting habits in a simpler and engaging way.</h4><br>
+    <h4>The purpose of this website is to provide people with the motivation to build meaningful and 
+        lasting habits through engaging with an animal of your choice.</h4><br>
     <h2>Meet our team!</h2>
     <h3>Sophia- Sophia is awesome and likes pink</h3>
     <h3>Paige- Paige hates coding and loves eating</h3>
     <h3>Kyleah- Kyleah is the best!</h3>
-    <h3>Emma- Emma is super smart and hardworking</h3>
+    <h3>Emma- Coding is a recent passion of mine that I gained from being forced to join a random elective at school. This world is newer to me, but I am happy to learn all that I can, which is why I chose to join this program and stick with it to the very end of this project.</h3>
 
 </body>
 </html>

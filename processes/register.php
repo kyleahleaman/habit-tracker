de457b183563cf0040f5fc1c877cd5b796bde14d
Kyleah Leaman <kyleah.leaman@gmail.com>
2026-08-03 13:23:53 -0400

    added css to register page

--- processes/register.php
+++ processes/register.php
@@ -3,8 +3,54 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
+        <style>
+            body {
+                background-color: #91c7b1;
+                font-family: Arial, Helvetica, sans-serif;
+            }
+
+            form {
+                margin-top: 20px;
+                border-radius: 15px;
+                box-shadow: 0 0 10px rgb(0, 0, 0, 0.2);
+                padding: 20px;
+                width: 30%;
+                background-color: white;
+                margin: auto;
+            }
+
+            input[type=text], input[type=password] {
+                width: 100%;
+                padding: 12px 20px;
+                margin: 8px 0;
+                display: inline-block;
+                border: 1px #5a5a66;
+                border-style: solid;
+                box-sizing: border-box;
+            }
+
+            input:hover[type="text"], input:hover[type="password"]{
+                background-color: #ddeaee;
+            }
+
+            button {
+                background-color: #91c7b1;
+                color: white;
+                padding: 14px 20px;
+                margin: 8px 0;
+                border: none;
+                cursor: pointer;
+                width: 100%;
+                border-radius: 15px;
+            }
+
+            button:hover {
+                opacity: 0.8;
+            }
+        </style>
 </head>
 <body>
+    <br><br><br><br>
     <div class = "container">
         <form name = "register" method = "post" id = "register" action = "../surveyPage.html">
             <h1>Register</h1>
@@ -36,10 +82,3 @@
         }
     </script>
 </body>
-<<<<<<< HEAD
-</html>
-=======
-</html> -->
-
-<?php echo 'here at register page'; ?>
->>>>>>> f5524dd (added login css)

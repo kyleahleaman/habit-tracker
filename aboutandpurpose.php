3d7a07a59b045e31a147258cb3f00e4663402a3c
paigehershey <hershey2@etown.edu>
2026-08-03 13:07:48 -0400

    Update aboutandpurpose.php
    changed my info

--- aboutandpurpose.php
+++ aboutandpurpose.php
@@ -14,7 +14,8 @@ include "navbar.php";
         lasting habits through engaging with an animal of your choice.</h4><br>
     <h2>Meet our team!</h2>
     <h3>Sophia- Sophia is awesome and likes pink</h3>
-    <h3>Paige- Paige hates coding and loves eating</h3>
+    <h3>Paige- This class has been my first experience with coding. It has been a huge challenge for me, 
+        but I am so glad that I took this opportunity. I hope that this habit tracker can be useful and helpful.</h3>
     <h3>Kyleah- Kyleah is the best!</h3>
     <h3>Emma- Coding is a recent passion of mine that I gained from being forced to join a random 
         elective at school. This world is newer to me, but I am happy to learn all that I can, which is 

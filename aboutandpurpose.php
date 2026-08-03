0dcf8f63bf65df448b7550766b5b78db09d95d7a
paigehershey <hershey2@etown.edu>
2026-08-03 10:10:19 -0400

    //ABOUT PAGE

---
+++ aboutandpurpose.php
@@ -0,0 +1,26 @@
+<?php
+include "header.php";
+?>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Our Purpose</title>
+    <link rel="stylesheet" href="habit.css">
+</head>
+<body>
+    <h1>Our Purpose</h1>
+    <h4>The purpose of this website it to build meaningful, 
+        lasting habits in a simpler and engaging way.</h4><br>
+    <h2>Meet our team!</h2>
+    <h3>Sophia- Sophia is awesome and likes pink</h3>
+    <h3>Paige- Paige hates coding and loves eating</h3>
+    <h3>Kyleah- Kyleah is the best!</h3>
+    <h3>Emma- Emma is super smart and hardworking</h3>
+
+</body>
+</html>
+
+<?php
+include "footer.php";
+?>

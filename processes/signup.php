70adaae7255140de7641cf689fb22a59b1126109
Emma Lang <LANG@etown.edu>
2026-08-04 10:12:17 -0400

    completed register into database

--- processes/signup.php
+++ processes/signup.php
@@ -1,27 +1,28 @@
 <?php
-require_once('dbconfig.php');
+    session_start();
+    require_once('dbconfig.php');
 
-$user = $_POST['username'];
-$pass = $_POST['password'];
-$cpass = $_POST['cpassword'];
+    $_SESSION["user"] = $_POST['username'];
+    $pass = $_POST['password'];
+    $cpass = $_POST['cpassword'];
 
-// connect to my database
-$conn = new mysqli($servername, $username, $password, $database);
+    // connect to my database
+    $conn = new mysqli($servername, $username, $password, $database);
 
-// check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+    // check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
 
-// prepare the SQL message
-$sql = "INSERT INTO users(username, bestpassword, coins, approved, createdOn) 
-VALUES ('{$user}', SHA2(CONCAT('salt', '{$pass}', 'pepper'), 0), 0, 1, NOW());";
+    // prepare the SQL message
+    $sql = "INSERT INTO users(username, bestpassword, coins, approved, createdOn) 
+    VALUES ('{$_SESSION["user"]}', SHA2(CONCAT('salt', '{$pass}', 'pepper'), 0), 0, 1, NOW());";
 
-// send this SQL message
-$stmt = $conn->prepare($sql);
-$stmt->execute();
-$conn->close();
+    // send this SQL message
+    $stmt = $conn->prepare($sql);
+    $stmt->execute();
+    $conn->close();
 
-header('location:../surveyPage.html')
+    header('location:../surveyPage.html')
 
 ?>
--- processes/survey.php
+++ processes/survey.php
@@ -1,13 +1,13 @@
 <?php
+    session_start();
     require_once('dbconfig.php');
-    include_once('signup.php');
 
-    $avatar = $_POST['avatar'];
+    $avatar = $_POST['character'];
     $age = $_POST['age'];
-    $sleep = $_POST['sleep'];
-    $exercise = $_POST['exercise'];
-    $water = $_POST['water'];
-    $screen = $_POST['screen'];
+    $sleep = isset($_POST['sleep']);
+    $exercise = isset($_POST['exercise']);
+    $water = isset($_POST['water']);
+    $screen = isset($_POST['screen']);
 
     // connect to my database
     $conn = new mysqli($servername, $username, $password, $database);
@@ -16,22 +16,22 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
-
+    
     // prepare the SQL message
     $sql = "UPDATE users
-        SET avater = $avater,
-        age = $age,
-        sleep = $sleep,
-        exercise = $exercise,
-        screen = $screen,
-        water = $water
-        WHERE username = '{$user}';";
+        SET avater = '{$avatar}',
+        age = '{$age}',
+        sleep = '{$sleep}',
+        exercise = '{$exercise}',
+        screen = '{$screen}',
+        water = '{$water}'
+        WHERE username = '{$_SESSION["user"]}';";
 
     // send this SQL message
     $stmt = $conn->prepare($sql);
     $stmt->execute();
     $conn->close();
 
-    header('../index.php?message=Thank you for signing up. Please log in.')
+    header('location:../index.php?message=Thank you for signing up. Please log in.')
 
 ?>

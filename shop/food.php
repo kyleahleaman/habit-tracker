65537240dcea7f4b78aa5bf65ec4f6b53e3025c9
paigehershey <hershey2@etown.edu>
2026-08-04 11:19:01 -0400

    all the shop files

---
+++ shop/food.php
@@ -0,0 +1,90 @@
+<?php
+include "shopnav.php"
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hat Store</title>
+    <link rel="stylesheet" href="flexbox.css">
+</head>
+<body>
+
+<h1>Food Store</h1>
+
+<div class="gallery">
+
+    <div class="card">
+    <img src= "images\food\alfarismaf-cake-8114265_1920.png" width= "50" height = "auto" alt = "Cake slice">
+        <h3>Cake</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\barelydevi-carrot-7545983_1920.png" width= "50" height = "auto" alt = "Carrot">
+        <h3>Carrot</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\clker-free-vector-images-hamburger-31775_1920.png" width= "50" height = "auto" alt = "Cheeseburger">
+        <h3>Cheeseburger</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\clker-free-vector-images-pizza-306495_1920.png" width= "50" height = "auto" alt = "Pizza">
+        <h3>Pizza</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\drabbitod-cookie-6685403_1920.png" width= "50" height = "auto" alt = "Cookie">
+        <h3>Cookie</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\flutie8211-apple-7376110_1920.png" width= "50" height = "auto" alt = "Apple">
+        <h3>Apple</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    
+    <div class="card">
+    <img src= "images\food\ideativas-tlm-taco-8029161_1920.png" width= "50" height = "auto" alt = "taco">
+        <h3>Taco</h3>
+        <h4>5 coins</h4>
+    </div>
+    
+    <div class="card">
+    <img src= "images\food\katamaheen-milkshake-6931083_1920.png" width= "50" height = "auto" alt = "milkshake">
+        <h3>Milkshake</h3>
+        <h4>5 coins</h4>
+    </div>
+    
+    <div class="card">
+    <img src= "images\food\sukariui-chicken-nuggets-5883662_1920.png" width= "50" height = "auto" alt = "Chicken Nuggets">
+        <h3>Chicken Nuggets</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\tkaucic-french-fries-2819957_1920.png" width= "50" height = "auto" alt = "French Fries">
+        <h3>French Fries</h3>
+        <h4>5 coins</h4>
+    </div>
+
+    <div class="card">
+    <img src= "images\food\clker-free-vector-images-sushi-35212_1920.png" width= "50" height = "auto" alt = "Sushi">
+        <h3>Sush</h3>
+        <h4>50 coins</h4>
+    </div>
+</div>
+
+</body>
+</html>
+<?php
+include "footer.php";
+?>
---
+++ shop/shopnav.php
@@ -0,0 +1,11 @@
+<div class="w3-top">
+        <div class="w3-bar w3-white w3-padding w3-card" style="letter-spacing:4px;">
+          <a href="profile.php" class="w3-bar-item w3-button">Profile</a>
+          <div class="w3-right w3-hide-small">
+            <a href="homepage.php" class="w3-bar-item w3-button">Home</a>
+            <a href="shop.php" class="w3-bar-item w3-button">Shop Menu</a>
+            <a href="decor.php" class="w3-bar-item w3-button">Decor</a>
+            <a href="hats.php" class="w3-bar-item w3-button">Hats</a>
+          </div>
+        </div>
+      </div>

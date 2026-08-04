3797e95beed94b2ba73c4288785b6c31b2962ace
Emma Lang <LANG@etown.edu>
2026-08-04 12:56:35 -0400

    updated dbconfig to fit new database

--- processes/dbconfig.php
+++ processes/dbconfig.php
@@ -1,6 +1,6 @@
 <?php 
     $servername = 'srv1482.hstgr.io'; 
-    $username = 'u121755072_habbittracker';
+    $username = 'u121755072_habittracker';
     $password = 'Q7g:0|Km6';
-    $database = 'u121755072_habbitdb';
+    $database = 'u121755072_habittrackerdb';
 ?>

68e505c138c4721ad0d5f4de6910067a8b14e1fe
Kyleah Leaman <kyleah.leaman@gmail.com>
2026-08-04 11:28:02 -0400

    bug fixes

--- navbar.php
+++ navbar.php
@@ -40,6 +40,7 @@
     <header>
         <nav>
             <ul>
+                <?PHP if($_SERVER["SERVER_ADDR"]=="127.0.0.1"){ ?>
                     <li><a href="/habit-tracker/homepage.php"><img src="/habit-tracker/images/home.png" width="75%"></a></li>
                     <li><a href="/habit-tracker/shop.php"><img src="/habit-tracker/images/shop.png" width="75%"></a></li>
                     <li><a href="/habit-tracker/profile.php"><img src="/habit-tracker/images/profile.png" width="75%"></a><li>
@@ -51,6 +52,21 @@
                         <a href="/habit-tracker/surveyPage.html">Habits</a>
                         <a href="/habit-tracker/processes/logout.php">Logout</a>
                     </div>
+
+
+                <?PHP }else{ ?>
+                    <li><a href="/homepage.php"><img src="/images/home.png" width="75%"></a></li>
+                    <li><a href="/shop.php"><img src="/images/shop.png" width="75%"></a></li>
+                    <li><a href="/profile.php"><img src="/images/profile.png" width="75%"></a><li>
+                    <button type="button" class="collapsible">▼</button>
+
+                    <div class="content-hidden">
+                        <a href="/profile.php">Profile</a>
+                        <a href="/shop.php">Shop</a>
+                        <a href="/surveyPage.html">Habits</a>
+                        <a href="/processes/logout.php">Logout</a>
+                    </div>
+                <?PHP }; ?>
             </ul>
         </nav>
     </header>

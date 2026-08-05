3e0c84fa865931d1be7a72cc34d6cdf71dd48111
Kyleah Leaman <kyleah.leaman@gmail.com>
2026-08-05 13:09:59 -0400

    added header and footer to profile

--- profile.php
+++ profile.php
@@ -1,1 +1,14 @@
-// add message for amount of sleep needed
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile: Habit Tracker</title>
+</head>
+<body>
+    <?php include 'navbar.php'; ?>
+
+
+    
+    <?php include 'footer.php'; ?>
+</body>
+</html>

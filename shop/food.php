8427490828f5a60e992c074f92ecb837a0acf814
Kyleah Leaman <kyleah.leaman@gmail.com>
2026-08-06 15:53:18 -0400

    updated store

--- shop/food.php
+++ shop/food.php
@@ -16,14 +16,14 @@
     <div class="card">
     <img src= "images/food/cake.png" width= "50" height = "auto" alt = "Cake slice">
         <h3>Cake</h3>
-        <h4>5 coins</h4>
+        <h4>50 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "cake">Buy</button>
     </div>
 
     <div class="card">
     <img src= "images/food/apple.png" width= "50" height = "auto" alt = "Apple">
         <h3>Apple</h3>
-        <h4>5 coins</h4>
+        <h4>50 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "apple">Buy</button>
     </div>
 
@@ -31,14 +31,14 @@
     <div class="card">
     <img src= "images/food/taco.png" width= "50" height = "auto" alt = "taco">
         <h3>Taco</h3>
-        <h4>5 coins</h4>
+        <h4>50 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "taco">Buy</button>
     </div>
     
     <div class="card">
     <img src= "images/food/milkshake.png" width= "50" height = "auto" alt = "milkshake">
         <h3>Milkshake</h3>
-        <h4>5 coins</h4>
+        <h4>50 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "milkshake">Buy</button>
     </div>
 
@@ -46,7 +46,7 @@
     <div class="card">
     <img src= "images/food/sushi.png" width= "50" height = "auto" alt = "Sushi">
         <h3>Sushi</h3>
-        <h4>5 coins</h4>
+        <h4>50 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "sushi">Buy</button>
     </div>
 </div>

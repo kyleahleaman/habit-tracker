5955c4ccd3e8d3d0dfc06fa778ade7c4646079b9
Kyleah Leaman <kyleah.leaman@gmail.com>
2026-08-06 13:52:49 -0400

    Revert "fixed store bugs"
    This reverts commit 8cea595850e944aab58266d14521bdbbd19c763b.

--- shop/food.php
+++ shop/food.php
@@ -5,40 +5,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Hat Store</title>
-    <style>
-        body{
-            background-color:#fbfffd;
-            font-family: Arial, Helvetica, sans-serif;
-        }
-
-        h1{
-            text-align: center;
-        }
-
-        .gallery{
-            display: flex;
-            flex-wrap: wrap;
-            justify-content: center; /*to show items in same space evenly, centered in the role wo spaces*/
-            gap: 20px; /*space btwn cards*/
-        }
-
-        .card{
-            width: 200px;
-            background-color:#91c7b1;
-            border-radius: 10px;
-            padding: 10px;
-            text-align: center;
-            box-shadow: 0 2px 8px #739f8e;
-            display: flex;
-            flex-direction: column;
-        }
-
-        .card img{
-            width: 100%;
-            border-radius: 8px;
-
-        }
-    </style>
+    <link rel="stylesheet" href="flexbox.css">
 </head>
 <body>
 <br>
@@ -47,14 +14,14 @@
 <div class="gallery">
 
     <div class="card">
-    <img src= "images/food/cake.png" width= "50" height = "auto" alt = "Cake slice">
+    <img src= "images\food\alfarismaf-cake-8114265_1920.png" width= "50" height = "auto" alt = "Cake slice">
         <h3>Cake</h3>
         <h4>5 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "cake">Buy</button>
     </div>
 
     <div class="card">
-    <img src= "images/food/apple.png" width= "50" height = "auto" alt = "Apple">
+    <img src= "images\food\flutie8211-apple-7376110_1920.png" width= "50" height = "auto" alt = "Apple">
         <h3>Apple</h3>
         <h4>5 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "apple">Buy</button>
@@ -62,14 +29,14 @@
 
     
     <div class="card">
-    <img src= "images/food/taco.png" width= "50" height = "auto" alt = "taco">
+    <img src= "images\food\ideativas-tlm-taco-8029161_1920.png" width= "50" height = "auto" alt = "taco">
         <h3>Taco</h3>
         <h4>5 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "taco">Buy</button>
     </div>
     
     <div class="card">
-    <img src= "images/food/milkshake.png" width= "50" height = "auto" alt = "milkshake">
+    <img src= "images\food\katamaheen-milkshake-6931083_1920.png" width= "50" height = "auto" alt = "milkshake">
         <h3>Milkshake</h3>
         <h4>5 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "milkshake">Buy</button>
@@ -77,7 +44,7 @@
 
 
     <div class="card">
-    <img src= "images/food/sushi.png" width= "50" height = "auto" alt = "Sushi">
+    <img src= "images\food\clker-free-vector-images-sushi-35212_1920.png" width= "50" height = "auto" alt = "Sushi">
         <h3>Sushi</h3>
         <h4>5 coins</h4>
         <button class = "store" onclick = "buy(event)" id = "sushi">Buy</button>
